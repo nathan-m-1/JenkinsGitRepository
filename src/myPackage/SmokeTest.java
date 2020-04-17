72fc6af7b526613adb20ad08f6ef7dd6e9db17b3
nathan <seromgn@hotmail.com>
2020-04-16 19:47:13 -0500

    first commit

---
+++ src/myPackage/SmokeTest.java
@@ -0,0 +1,28 @@
+package myPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+
+	
+	@Test
+	public void loginTest() throws InterruptedException{
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/lib/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http:\\rediff.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("//*[@id='signin_info']/a[1]")).click();
+		
+		
+		Thread.sleep(5000);
+		
+		driver.quit();
+	}
+}
